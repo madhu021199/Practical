f06ea89bf7392969b349df1423620c45067ad8f4
ram <rmadhumitha21199@gmail.com>
2024-11-22 21:15:43 +0530

    Practical Aus

--- src/main/java/excellearning/GitExample.java
+++ src/main/java/excellearning/GitExample.java
@@ -1,11 +1,13 @@
 package excellearning;
 
+
 public class GitExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("good morning");
+		System.out.println("Good Night");
 	}
 
 }
